aa4896de3245a664ac4d28cd209915d9f92539e8
Steven717333 <stevenlee313777@gmail.com>
2025-12-26 21:24:47 +0700

    Commit No. 1 – add data binding

--- app/src/main/java/com/example/test_lab_week_12/MainActivity.kt
+++ app/src/main/java/com/example/test_lab_week_12/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.test_lab_week_12
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
@@ -11,9 +12,11 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.example.test_lab_week_12.databinding.ActivityMainBinding
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
     private val movieAdapter by lazy {
         MovieAdapter(object : MovieAdapter.MovieClickListener {
             override fun onMovieClick(movie: Movie) {
@@ -24,62 +27,27 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val recyclerView: RecyclerView = findViewById(R.id.movie_list)
+
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val recyclerView: RecyclerView = binding.movieList
         recyclerView.adapter = movieAdapter
+
         val movieRepository = (application as MovieApplication).movieRepository
+
         val movieViewModel = ViewModelProvider(
-            this, object : ViewModelProvider.Factory {
+            this,
+            object : ViewModelProvider.Factory {
                 override fun <T : ViewModel> create(modelClass: Class<T>): T {
                     return MovieViewModel(movieRepository) as T
                 }
-            })[MovieViewModel::class.java]
-//        movieViewModel.popularMovies.observe(this) { popularMovies ->
-//            val currentYear =
-//                Calendar.getInstance().get(Calendar.YEAR).toString()
-//            movieAdapter.addMovies(
-//                popularMovies
-//                    .filter { movie ->
-//// aman dari null
-//                        movie.releaseDate?.startsWith(currentYear) == true
-//                    }
-//                    .sortedByDescending { it.popularity }
-//            )
-//        }
-//        movieViewModel.error.observe(this) { error ->
-//            if (error.isNotEmpty()) {
-//                Snackbar.make(recyclerView, error, Snackbar.LENGTH_LONG).show()
-//            }
-//        }
-
-        // fetch movies from the API
-// lifecycleScope is a lifecycle-aware coroutine scope
-        lifecycleScope.launch {
-// repeatOnLifecycle is a lifecycle-aware coroutine builder
-// Lifecycle.State.STARTED means that the coroutine will run
-// when the activity is started
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                launch {
-// collect the list of movies from the StateFlow
-                    movieViewModel.popularMovies.collect {
-// add the list of movies to the adapter
-                            movies ->
-                        movieAdapter.addMovies(movies)
-                    }
-                }
-                launch {
-// collect the error message from the StateFlow
-                    movieViewModel.error.collect { error ->
-// if an error occurs, show a Snackbar  with the error message
-
-                        if (error.isNotEmpty()) Snackbar
-                            .make(
-                                recyclerView, error, Snackbar.LENGTH_LONG
-                            ).show()
-                    }
-                }
             }
-        }
+        )[MovieViewModel::class.java]
+
+
+        binding.viewModel = movieViewModel
+        binding.lifecycleOwner = this
     }
 
     private fun openMovieDetails(movie: Movie) {
